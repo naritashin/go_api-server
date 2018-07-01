2d82bd30b929078a5cace9a74da658a53914ab7a
Shin Narita <triplfjump1417@gmail.com>
2018-07-02 01:17:50 +0900

    goまわり追加

--- app.go
+++ app.go
@@ -1,1 +1,12 @@
-// golang
+package main
+
+import (
+	_ "github.com/labstack/echo"
+	_ "github.com/labstack/echo/middleware"
+
+	router "github.com/naritashin/go_api-server/controller"
+)
+
+func main() {
+	router.Run()
+}
